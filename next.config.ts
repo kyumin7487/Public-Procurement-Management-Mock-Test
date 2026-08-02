41a9a8eecbac5a3605b10b2f5671b3b8123f4bcb
박규민 <131234408+kyumin7487@users.noreply.github.com>
2026-08-02 20:51:09 +0900

    정적배포를 위한 next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
+const repositoryName =
+    process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+
+const basePath = isGitHubPages
+    ? `/${repositoryName}`
+    : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    trailingSlash: true,
+    basePath,
+
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
